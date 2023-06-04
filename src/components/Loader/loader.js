91d5127b5e23e652e120293038f536e1e07fef6f
guillaumebrethes <herver86@gmail.com>
2023-06-04 22:31:22 +0200

    🚧 work in progress✨: loader OK on page survey

---
+++ src/components/Loader/loader.js
@@ -0,0 +1,14 @@
+//  === Import  ===;
+
+//  === Import : local  ===;
+import './loader.scss';
+//  === Component ===;
+const Loader = () => {
+  console.log('Composant Loader => ', Loader);
+
+  return (
+    <div className="loader" />
+  );
+};
+
+export default Loader;
--- src/components/pages/Survey/survey.js
+++ src/components/pages/Survey/survey.js
@@ -5,6 +5,7 @@ import { Link, useParams } from 'react-router-dom';
 //  === Import : local  ===;
 import './survey.scss';
 import { useEffect, useState } from 'react';
+import Loader from '../../Loader/loader';
 
 //  === Component ===;
 const Survey = () => {
@@ -15,20 +16,28 @@ const Survey = () => {
   const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1;
   const nextQuestionNumbre = questionNumberInt + 1;
 
-  const [questionApi, setQuestionApi] = useState({});
   const [surveyData, setSurveyData] = useState({});
+  const [isDataLoading, setDataLoading] = useState(false);
 
   useEffect(() => {
-    // setDataLoading(true);
+    setDataLoading(true);
     fetch('http://localhost:8000/survey')
       .then((res) => res.json()
-        .then(({ surveyData }) => setSurveyData(surveyData))
-        .catch((error) => console.log(error)));
+        .catch((error) => console.log(error))
+        .then(({ surveyData }) => {
+          setSurveyData(surveyData);
+          setDataLoading(false);
+        }));
   }, []);
+
   return (
     <div className="survey">
       <h1>Question n°{questionNumber} </h1>
-      <h2> {surveyData[questionNumber]} </h2>
+      {isDataLoading ? (
+        <Loader />
+      ) : (
+        <h2> {surveyData[questionNumber]} </h2>
+      )}
       <div className="survey__links">
         <Link to={`/survey/${prevQuestionNumber}`}> Précédent </Link>
         {
